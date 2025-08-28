54ebac6901ff4bae0152832a8d9cbe55cd337f34
Austiniaco2 <austinbrian358@gmail.com>
2025-08-28 06:31:50 -1100

    added the jwt in the .env variables

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,10 @@
-// src/api/axios.js
-import axios from "axios";
+  // src/api/axios.js
+  import axios from "axios";
 
-// Create an instance of axios with the base URL of your backend
-const apiClient = axios.create({
-  // baseURL: 'https://rocimuc-api.onrender.com',
-  baseURL: "http://localhost:5000",
-});
+  // Create an instance of axios with the base URL of your backend
+  const apiClient = axios.create({
+    // baseURL: 'https://rocimuc-api.onrender.com',
+    baseURL: "http://localhost:5000",
+  });
 
-export default apiClient;
+  export default apiClient;
