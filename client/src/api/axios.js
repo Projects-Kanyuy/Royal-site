1f34a30a26a4cd35c52e3fa4c9c212ae6010d962
Rouclec <senatorasonganyi97@gmail.com>
2025-10-25 19:08:25 +0100

    hardcoding backend base url on client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 // Create an instance of axios with the base URL of your backend
 const apiClient = axios.create({
-  // baseURL: 'https://rocimuc-api.onrender.com',
-  baseURL: process.env.REACT_APP_SERVER_BASE_URL,
+  // baseURL: process.env.REACT_APP_SERVER_BASE_URL,
+  baseURL: "https://api.royalcitysnack.site",
 });
 
 export default apiClient;
