9de92931d92b88dda89d3d3da50080e4bcb1d56d
ndip123 <nojong23@gmail.com>
2025-12-03 18:54:03 +0100

    for update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an instance of axios with the base URL of your backend
 const apiClient = axios.create({
-  baseURL: 'https://rocimuc-api.onrender.com',
+  baseURL: 'http://13.60.186.10:5000',
 });
 
 export default apiClient;
